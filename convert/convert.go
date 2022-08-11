d351cb1387836287dd00730279bfffa39a2ecaaf
Phil Kedy <phil.kedy@gmail.com>
2022-08-11 10:18:53 -0400

    Add Nillable, Parse, and StringPtr to convert package

--- convert/convert.go
+++ convert/convert.go
@@ -1,10 +1,41 @@
 package convert
 
+import (
+	"fmt"
+)
+
 const Package = "Convert"
 
 //go:inline
 func Ptr[T any](value T) *T {
 	return &value
+}
+
+func Nillable[S, D any](value *S, convert func(S) D) *D {
+	if value == nil {
+		return nil
+	}
+	ret := convert(*value)
+	return &ret
+}
+
+func Parse[S, D any](value *S, parse func(S) (D, error)) (*D, error) {
+	if value == nil {
+		return nil, nil
+	}
+	ret, err := parse(*value)
+	if err != nil {
+		return nil, err
+	}
+	return &ret, nil
+}
+
+func StringPtr[S fmt.Stringer](value *S) *string {
+	if value == nil {
+		return nil
+	}
+	ret := (*value).String()
+	return &ret
 }
 
 func Slice[S, D any](src []S, convert func(S) D) []D {
